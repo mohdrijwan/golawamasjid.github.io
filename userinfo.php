a7acd7a4f45bda51ae445d69aa93449dced6ec35
mohdrijwan <52716117+mohdrijwan@users.noreply.github.com>
2021-03-02 07:13:07 +0530

    publishing my first website
    wow i am happy

---
+++ userinfo.php
@@ -0,0 +1,17 @@
+<?php
+$con=mysqli_connect("localhost","root");
+if($con){
+	echo "success";
+}else{
+	echo "Fail";
+}
+mysqli_select_db($con,'masjid');
+$user=$_POST['user'];
+$email=$_POST['email'];
+$mobile=$_POST['mobile'];
+$comment=$_POST['comment'];
+
+$query="insert into userinfodata (user,email,mobile,comment) values('$user','$email','$mobile','$comment')";
+mysqli_query($con,$query);
+header('location:index.php');
+?>
